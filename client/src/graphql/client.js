1bc5599a9e6c378b889a5197f977edfa9c67206d
Yadlapalli Aditya Ram <adityaramyadlapalli@gmail.com>
2026-05-28 15:01:49 +0530

    Fix GraphQL endpoint URL parsing for production environment variables

--- client/src/graphql/client.js
+++ client/src/graphql/client.js
@@ -1,8 +1,12 @@
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
+const baseUrl = import.meta.env.VITE_API_URL 
+  ? import.meta.env.VITE_API_URL.replace(/\/api\/?$/, '') 
+  : 'http://localhost:5000';
+
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: import.meta.env.VITE_API_URL ? `${import.meta.env.VITE_API_URL}/graphql` : 'http://localhost:5000/graphql',
+    uri: `${baseUrl}/graphql`,
   }),
   cache: new InMemoryCache(),
 });
