25c7ecdaf07463d8910c355bef752fbf94f70d3a
Yadlapalli Aditya Ram <adityaramyadlapalli@gmail.com>
2026-05-28 14:12:40 +0530

    Remove .env file from tracking and update gitignore

--- client/src/graphql/client.js
+++ client/src/graphql/client.js
@@ -1,8 +1,6 @@
-import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-  link: new HttpLink({
-    uri: import.meta.env.VITE_API_URL ? `${import.meta.env.VITE_API_URL}/graphql` : 'http://localhost:5000/graphql',
-  }),
+  uri: import.meta.env.VITE_API_URL ? `${import.meta.env.VITE_API_URL}/graphql` : 'http://localhost:5000/graphql',
   cache: new InMemoryCache(),
 });
